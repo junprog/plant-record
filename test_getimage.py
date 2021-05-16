c942c9d538f117e80520b3f04f8395636f772642
Junya Uchida <jyunya.uchida@gmail.com>
2021-05-17 04:56:12 +0900

    微修正

--- test_getimage.py
+++ test_getimage.py
@@ -1,15 +1,4 @@
-import json
-
 import get_image as getimg
-import notify
 
 ## 撮影
 img_path = getimg.captureImage()
-
-## twitter 投稿
-twitter_response = notify.tweetInfo(image_filepath=img_path)
-twitter_response = json.loads(twitter_response)
-twitter_url = twitter_response["entities"]["media"][0]["expanded_url"]
-
-## slack 通知
-notify.notifySlack(twitter_url=twitter_url)
