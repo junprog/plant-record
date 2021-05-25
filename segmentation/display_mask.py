49e81f809979779b8019376ed7d5979c8c7cb1f2
Junya Uchida <jyunya.uchida@gmail.com>
2021-05-25 21:54:44 +0900

    コミット

--- segmentation/display_mask.py
+++ segmentation/display_mask.py
@@ -2,16 +2,20 @@ import json
 import numpy as np
 import labelme.utils as utils
 
-with open('../images/E14h481VcAUUa1t.json', "r",encoding="utf-8") as f:
+with open('C:/Users/Junya/Desktop/dataset/20210521_184321402_iOS.json', "r",encoding="utf-8") as f:
     dj = json.load(f)
 
 mask_branch = utils.shape_to_mask((dj['imageHeight'],dj['imageWidth']), dj['shapes'][0]['points'], shape_type=None,line_width=1, point_size=1)
 mask_pot = utils.shape_to_mask((dj['imageHeight'],dj['imageWidth']), dj['shapes'][1]['points'], shape_type=None,line_width=1, point_size=1)
-mask_leaf = utils.shape_to_mask((dj['imageHeight'],dj['imageWidth']), dj['shapes'][2]['points'], shape_type=None,line_width=1, point_size=1)
+mask_leaf0 = utils.shape_to_mask((dj['imageHeight'],dj['imageWidth']), dj['shapes'][2]['points'], shape_type=None,line_width=1, point_size=1)
+mask_leaf1 = utils.shape_to_mask((dj['imageHeight'],dj['imageWidth']), dj['shapes'][3]['points'], shape_type=None,line_width=1, point_size=1)
+mask_leaf2 = utils.shape_to_mask((dj['imageHeight'],dj['imageWidth']), dj['shapes'][4]['points'], shape_type=None,line_width=1, point_size=1)
 
 mask_branch = mask_branch.astype(np.int)[:,:,np.newaxis]*255#booleanを0,1に変換
 mask_pot = mask_pot.astype(np.int)[:,:,np.newaxis]*255#booleanを0,1に変換
-mask_leaf = mask_leaf.astype(np.int)[:,:,np.newaxis]*255#booleanを0,1に変換
+mask_leaf = mask_leaf0.astype(np.int)[:,:,np.newaxis]*255#booleanを0,1に変換
+mask_leaf += mask_leaf1.astype(np.int)[:,:,np.newaxis]*255#booleanを0,1に変換
+mask_leaf += mask_leaf2.astype(np.int)[:,:,np.newaxis]*255#booleanを0,1に変換
 
 mask_img = np.concatenate([mask_branch, mask_pot,mask_leaf], axis=2)
 
