47cc4c313854930d205330c2331ad82ac58ce121
Ryosuke <re0053pr@ed.ritsumei.ac.jp>
2021-06-02 11:01:54 +0900

    load_dataset整理，データセット追加

--- segmentation/load_dataset.py
+++ segmentation/load_dataset.py
@@ -3,30 +3,6 @@ import numpy as np
 import tensorflow as tf
 from glob import glob
 
-
-AUTOTUNE = tf.data.experimental.AUTOTUNE
-print(f"Tensorflow ver. {tf.__version__}")
-
-training_data = 'train/'
-test_data = 'test/'
-
-TRAINSET_SIZE = len(glob(training_data + "*.jpg"))
-print(f"The Training Dataset contains {TRAINSET_SIZE} images.")
-
-TESTSET_SIZE = len(glob(test_data + "*.jpg"))
-print(f"The Test Dataset contains {TESTSET_SIZE} images.")
-
-#random seed
-SEED = 30
-# Input size
-IMG_SIZE = 128
-#適当
-BATCH_SIZE = 1
-# シャッフルバッファのサイズをデータセットとおなじに設定することで、データが完全にシャッフルされる
-BUFFER_SIZE = TRAINSET_SIZE
-
-
-
 def parse_image(img_path: str) -> dict:
     img = tf.io.read_file(img_path)
     img = tf.image.decode_jpeg(img, channels=3)
@@ -45,11 +21,6 @@ def parse_image(img_path: str) -> dict:
     mask = tf.where(mask == 255, np.dtype('uint8').type(0), mask)
 
     return {'image': img, 'segmentation_mask': mask}
-
-train_dataset = tf.data.Dataset.list_files(training_data + "*.jpg", seed=SEED)
-train_dataset = train_dataset.map(parse_image)
-test_dataset = tf.data.Dataset.list_files(test_data + "*.jpg", seed=SEED)
-test_dataset =test_dataset.map(parse_image)
 
 
 @tf.function
@@ -82,21 +53,6 @@ def load_image_test(datapoint: dict) -> tuple:
 
     return input_image, input_mask
 
-
-#Create Dataset
-dataset = {"train": train_dataset, "test": test_dataset}
-
-dataset['train'] = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-dataset['train'] = dataset['train'].shuffle(buffer_size=BUFFER_SIZE, seed=SEED)
-dataset['train'] = dataset['train'].repeat()
-dataset['train'] = dataset['train'].batch(BATCH_SIZE)
-dataset['train'] = dataset['train'].prefetch(buffer_size=AUTOTUNE)
-
-dataset['test'] = dataset['test'].map(load_image_test)
-dataset['test'] = dataset['test'].repeat()
-dataset['test'] = dataset['test'].batch(BATCH_SIZE)
-dataset['test'] = dataset['test'].prefetch(buffer_size=AUTOTUNE)
-
 def display_sample(display_list):
 
     plt.figure(figsize=(18, 18))
@@ -110,7 +66,48 @@ def display_sample(display_list):
         plt.axis('off')
     plt.show()
 
-for image, mask in dataset['train'].take(1):
-    sample_image, sample_mask = image, mask
 
-display_sample([sample_image[0], sample_mask[0]])
+if __name__ == '__main__':
+
+    training_data = 'train/'
+    test_data = 'test/'
+    
+    TRAINSET_SIZE = len(glob(training_data + "*.jpg"))
+    print(f"The Training Dataset contains {TRAINSET_SIZE} images.")
+    TESTSET_SIZE = len(glob(test_data + "*.jpg"))
+    print(f"The Test Dataset contains {TESTSET_SIZE} images.")
+
+    # TODO:configファイル
+    SEED = 30
+    IMG_SIZE = 128
+    BATCH_SIZE = 1
+
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+    # print(f"Tensorflow ver. {tf.__version__}")
+
+    train_dataset = tf.data.Dataset.list_files(training_data + "*.jpg", seed=SEED)
+    train_dataset = train_dataset.map(parse_image)
+    test_dataset = tf.data.Dataset.list_files(test_data + "*.jpg", seed=SEED)
+    test_dataset =test_dataset.map(parse_image)
+
+
+    dataset = {"train": train_dataset, "test": test_dataset}
+
+    # シャッフルバッファのサイズをデータセットとおなじに設定することで、データが完全にシャッフルされる
+    BUFFER_SIZE = TRAINSET_SIZE
+
+    dataset['train'] = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+    dataset['train'] = dataset['train'].shuffle(buffer_size=BUFFER_SIZE, seed=SEED)
+    dataset['train'] = dataset['train'].repeat()
+    dataset['train'] = dataset['train'].batch(BATCH_SIZE)
+    dataset['train'] = dataset['train'].prefetch(buffer_size=AUTOTUNE)
+
+    dataset['test'] = dataset['test'].map(load_image_test)
+    dataset['test'] = dataset['test'].repeat()
+    dataset['test'] = dataset['test'].batch(BATCH_SIZE)
+    dataset['test'] = dataset['test'].prefetch(buffer_size=AUTOTUNE)
+
+    for image, mask in dataset['train'].take(1):
+        sample_image, sample_mask = image, mask
+
+    display_sample([sample_image[0], sample_mask[0]])
