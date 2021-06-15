a484cae6ed9be4ae7b71a7a3392b949438943c99
Junya Uchida <jyunya.uchida@gmail.com>
2021-06-15 22:59:27 +0900

    weight 削除

--- test/test_predict.py
+++ test/test_predict.py
@@ -8,7 +8,7 @@ import segmentation as seg
 if __name__ == '__main__':
     #test
     img_path = 'C:/Users/Junya/Downloads/89hjqj6arfb/20210521_184321402_iOS.jpg'
-    weight_path = 'weights/0615-211605-unet'
+    weight_path = 'weights/0615-225649-unet'
 
     mask_path, viz_path = seg.predict(img_path, weight_path)
     print(mask_path, viz_path)
